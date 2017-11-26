778d9a59062361fc0a11ccf5caaa3532b50644c2
Justin Rhude <justin.rhude52@gmail.com>
2017-11-26 12:25:40 -0500

    started adding actual gameplay

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,18 +7,26 @@ var action_points = 5
 var people
 var day = 1
 
+var firewall_bar
+
 func _ready():
 	people = get_tree().get_nodes_in_group("People")
+	firewall_bar = get_node("sprites_layer/stats_label/firewall_bar")
 	pass
-	
+
 func update_values():
 	var productivity_total = 0
 	for person in people:
+		
 		person.update_values()
 		productivity_total += person.productivity
 	productivity = productivity_total / people.size()
 	
 	pass
+	
+#func infect_people():
+	#for person in people:
+		#if person.
 
 func end_day():
 	update_values()
